c0602ac8acd1cee57c74db47aa15bba7044f5f6e
Mathis ROME <mathis.rome@icloud.com>
2023-04-17 00:29:47 +0200

    feat : permettre la sélection du nombre de quilles tombées

--- src/Command/PlayBowlingCommand.php
+++ src/Command/PlayBowlingCommand.php
@@ -3,13 +3,15 @@
 namespace App\Command;
 
 use App\Entity\Game;
-use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 #[AsCommand(
     name: 'app:play-bowling',
@@ -29,32 +31,46 @@ class PlayBowlingCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $output->writeln([
             'BOWLING',
-            '============',
-            '| | | |',
-            ' | | |',
-            '  | |',
-            '   |',
+            '=======',
         ]);
 
         $game = new Game();
 
+        $helper = $this->getHelper('question');
+        $questionFirstRollPins = new ChoiceQuestion('Nombre de quille tombé au premier coup ?', [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        ], 0);
+        $questionSecondRollPins = new ChoiceQuestion('Nombre de quille tombé au second coup ?', [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        ], 0);
+        $questionLastRollPins = new ChoiceQuestion('Nombre de quille tombé au dernier coup ?', [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+        ], 0);
+
+        $errorMessage = "Veuillez choisir un des choix possibles.";
+        $questionFirstRollPins->setErrorMessage($errorMessage);
+        $questionSecondRollPins->setErrorMessage($errorMessage);
+        $questionLastRollPins->setErrorMessage($errorMessage);
+
         for ($round = 1; $round <= 10; $round++) {
-            $firstRollPins = \rand(0, 10);
+            $output->writeln(\sprintf('Lancé numéro : %s', $round));
+
+            $firstRollPins = $helper->ask($input, $output, $questionFirstRollPins);
             $game->roll($firstRollPins);
 
             if ($firstRollPins < 10) {
-                $secondRollPins = \rand(0, 10 - $firstRollPins);
+                $secondRollPins = $helper->ask($input, $output, $questionSecondRollPins);
                 $game->roll($secondRollPins);
 
                 if ($round === 10 && $firstRollPins + $secondRollPins === 10) {
-                    $game->roll(\rand(0, 10));
+                    $game->roll($helper->ask($input, $output, $questionLastRollPins));
                 }
             } else {
                 if ($round === 10) {
-                    $firstRollPins = \rand(0, 10);
+                    $firstRollPins = $helper->ask($input, $output, $questionFirstRollPins);
                     $game->roll($firstRollPins);
 
-                    $secondRollPins = \rand(0, 10 - $firstRollPins);
+                    $secondRollPins = $helper->ask($input, $output, $questionSecondRollPins);
                     $game->roll($secondRollPins);
                 }
             }
